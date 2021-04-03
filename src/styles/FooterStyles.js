733ba3a3a4815d408649e3ad08265ad07502e587
Khalis Ab Rahman <khalisabrahman@gmail.com>
2021-04-03 11:45:35 +0800

    Reduced footerBox margin

--- src/styles/FooterStyles.js
+++ src/styles/FooterStyles.js
@@ -1,33 +1,32 @@
-const styles = ((theme) => ({
-    iconStyle: {
+const styles = (theme) => ({
+	iconStyle: {
 		width: '32px',
 		height: '32px',
 	},
 	footer: {
 		backgroundColor: '#28527a',
 		overflow: 'auto',
-		width: '100vw'
+		width: '100vw',
 	},
 	gutterMain: {
 		display: 'flex',
 		justifyContent: 'center',
-		// marginRight: '20px',
-		[theme.breakpoints.down('sm')]: {
-			// marginRight: '5px',
-		},
+
+		[theme.breakpoints.down('sm')]: {},
 	},
 	footerIcons: {
 		display: 'flex',
 	},
 	footerBox: {
-		margin: '30px',
-        '& a': {
-            color: 'inherit'
-        },
-        '& a:hover': {
-            color: '#f4d160'
-        }
+		margin: '20px',
+
+		'& a': {
+			color: 'inherit',
+		},
+		'& a:hover': {
+			color: '#f4d160',
+		},
 	},
-}));
+});
 
 export default styles;
